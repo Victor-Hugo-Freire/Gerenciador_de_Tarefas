d39d156ad685d3b0ecf7acbd84351bc2ebab564d
Victor-Hugo-Freire <victor.torugo.freire@gmail.com>
2025-03-03 19:06:44 -0300

    feat: the user can put a task as completed or incompleted by clicking on the task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,22 @@
+import { ChevronRightIcon } from "lucide-react";
+
 function Tasks(props) {
   return (
     <div>
       <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
         {props.tasks.map((task) => (
-          <li key={task.id} className="flex">
-            <button className="bg-slate-400 w-full text-white p-2 rounded-md">
+          <li key={task.id} className="flex gap-2">
+            <button
+              onClick={() => props.onTaskClick(task.id)}
+              className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+                task.isCompleted && "line-through"
+              }`}
+            >
               {task.title}
             </button>
-            <button>Ver Detalhes</button>
+            <button className="bg-slate-400 p-2 rounded-md text-white">
+              <ChevronRightIcon />
+            </button>
           </li>
         ))}
       </ul>
