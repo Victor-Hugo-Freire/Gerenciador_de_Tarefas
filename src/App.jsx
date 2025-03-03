a2def73c9ea44c16be9b43cf1136129a3553f71d
Victor-Hugo-Freire <victor.torugo.freire@gmail.com>
2025-03-03 18:46:16 -0300

    separating the tasks from the button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar",
+      descrption: "Estudar React",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Ir na Academia",
+      descrption: "Treinar alguma parte do corpo",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Levar o lixo",
+      descrption: "Para não acumular",
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 }
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,7 @@
 function AddTask() {
   return (
     <div>
-      <h1>AddTask</h1>
+      <h1></h1>
     </div>
   );
 }
