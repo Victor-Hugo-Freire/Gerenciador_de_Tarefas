075b211b14b9783bc6e9ee09508af0c73a68bb6c
Victor-Hugo-Freire <victor.torugo.freire@gmail.com>
2025-03-04 17:27:19 -0300

    add: add a icon of check when the task is completed

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -20,10 +20,13 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
           <li key={task.id} className="flex gap-2">
             <button
               onClick={() => onTaskClick(task.id)}
-              className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
                 task.isCompleted && "line-through"
               }`}
             >
+              {task.isCompleted && <CheckIcon />}
+              {/* OU(nesse caso o react não nos permite usar o if) */}
+              {/* {task.isCompleted ? <CheckIcon /> : null} */}
               {task.title}
             </button>
 
