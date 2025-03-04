e527be138166c35f4c6aefad23738aa2d6589cc0
Victor-Hugo-Freire <victor.torugo.freire@gmail.com>
2025-03-04 11:52:00 -0300

    feat: now the button of see details is working

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,17 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <div>
       <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
@@ -14,9 +25,14 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
             >
               {task.title}
             </button>
-            <button className="bg-slate-400 p-2 rounded-md text-white">
+
+            <button
+              onClick={() => onSeeDetailsClick(task)}
+              className="bg-slate-400 p-2 rounded-md text-white"
+            >
               <ChevronRightIcon />
             </button>
+
             <button
               onClick={() => onDeleteTaskClick(task.id)}
               className="bg-slate-400 p-2 rounded-md text-white"
